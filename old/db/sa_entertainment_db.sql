66c5a20ab48b4c17cf644c67a62cd44fb6024761
nayeemahoque <113884690+nayeemahoque@users.noreply.github.com>
2022-12-10 14:36:26 +0600

    Update sa_entertainment_db.sql

--- old/db/sa_entertainment_db.sql
+++ old/db/sa_entertainment_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2022 at 10:49 AM
+-- Generation Time: Dec 10, 2022 at 09:34 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
